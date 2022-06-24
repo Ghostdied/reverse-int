b667f67f778864d5268f8c6324608b642b7aef10
Ghostdied <ghostdied3@gmail.com>
2022-06-24 03:07:02 +0300

    feat: added implementation method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = ''
+    if (n < 0) {
+        return (n * -1).toString().split('').reverse().join('')
+    } else {
+        return n.toString().split('').reverse().join('')
+    }
+
 }
